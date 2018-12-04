b562f33e069a927e48d04fc0827ce85079d9e7de
ssrz1 <sajjadraza00@gmail.com>
2018-12-04 09:25:37 +0500

    React

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -1,10 +1,10 @@
 import React, { Component } from "react";
-
+import Ninja from "./ninja";
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1> APP! HURRAH</h1>
+        <Ninja name="Fawaz" age="26" belt="Black" />
       </div>
     );
   }
