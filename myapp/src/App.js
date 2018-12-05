451fc6d9a494af64efdd0eb549d2e16fe94f1c7b
ssrz1 <sajjadraza00@gmail.com>
2018-12-05 22:52:23 +0500

    React

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -1,17 +1,20 @@
 import React, { Component } from "react";
 import Ninja from "./ninja";
+import AddNinja from "./AddNinja";
 class App extends Component {
   state = {
     ninjas: [
       { name: "Sajjad", age: 25, belt: "red", id: 1 },
       { name: "Fawaz", age: 26, belt: "black", id: 2 },
-      { name: "Ali", age: 25, belt: "blue", id: 3 }
+      { name: "Ali", age: 11, belt: "blue", id: 3 }
     ]
   };
+  addNinja = () => {};
   render() {
     return (
       <div className="App">
         <Ninja ninjas={this.state.ninjas} />
+        <AddNinja />
       </div>
     );
   }
--- myapp/src/ninja.js
+++ myapp/src/ninja.js
@@ -1,19 +1,43 @@
 import React from "react";
+import AddNinja from "./AddNinja";
 //Child component
 const Ninja = props => {
   // console.log(this.props);
   const { ninjas } = props;
-  const ninjaList = ninjas.map(ninjas => {
-    return (
-      <div className="ninja">
-        <div>Name:{ninjas.name}</div>
-        <div>Age: {ninjas.age}</div>
+  //   const ninjaList = ninjas.map(ninja => {
+  //     if (ninja.age > 20) {
+  //       return (
+  //         <div className="ninja">
+  //           <div>Name:{ninja.name}</div>
+  //           <div>Age: {ninja.age}</div>
 
-        <div>Belt: {ninjas.belt}</div>
+  //           <div>Belt: {ninja.belt}</div>
+  //           <br />
+  //         </div>
+  //       );
+  //     } else {
+  //       return null;
+  //     }
+  //   });
+
+  const ninjaList = ninjas.map(ninja => {
+    return ninja.age > 20 ? (
+      <div className="ninja">
+        <div>Name:{ninja.name}</div>
+        <div>Age: {ninja.age}</div>
+
+        <div>Belt: {ninja.belt}</div>
+        <br />
       </div>
-    );
+    ) : null;
   });
-  return <div className="ninja-list">{ninjaList}</div>;
+
+  return (
+    <div className="ninja-list">
+      {ninjaList}
+      <AddNinja />
+    </div>
+  );
 };
 
 export default Ninja;
